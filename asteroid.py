b71d217f1d24de18a3aa807c726b9132364fd1b2
EachFlounder967 <jarrad.m.billiot@gmail.com>
2026-01-11 12:22:53 -0600

    Implement shooting cooldown and asteroid splitting

---
+++ asteroid.py
@@ -0,0 +1,38 @@
+import pygame
+from circleshape import CircleShape
+from constants import ASTEROID_MIN_RADIUS, LINE_WIDTH
+from logger import log_event
+import random
+
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(
+            screen,
+            "white",
+            ((int(self.position.x)), (int(self.position.y))),
+            self.radius,
+            LINE_WIDTH,
+        )
+
+    def update(self, dt):
+        self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+
+        log_event("asteroid_split")
+        angle = random.uniform(20, 50)
+        negative_angle = -angle
+        new_velocity_1 = self.velocity.rotate(angle)
+        new_velocity_2 = self.velocity.rotate(negative_angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid_1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_2 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_1.velocity = new_velocity_1 * 1.2
+        asteroid_2.velocity = new_velocity_2 * 1.2
